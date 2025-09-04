af2a58fa414c31e31b7c122cb92709ffc294913e
Kenney J Phillis Jr <kphillisjr@gmail.com>
2025-09-04 17:40:17 -0500

    restore folder archive-mar-31-09

---
+++ archive-mar-31-09/output/CollisionEvent.java
@@ -0,0 +1,11 @@
+public class CollisionEvent extends HeronObject {
+  // attributes
+  public double timeElapsed;
+  // operations
+  // state entry procedures
+  // dispatch function
+  public void onSignal(HeronSignal signal) {
+    if (false) {
+      }
+    }
+  }
